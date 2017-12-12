21f584f7fbb9b13ad8ec875a51b0998a8119cd87
WesselBruines <Wessel@MacBook-Pro-van-Wessel.local>
2017-12-12 15:20:10 +0100

    Eerste opzet van SEW - email template

---
+++ js/learning.js
@@ -0,0 +1,11 @@
+
+
+
+var nextStep = document.querySelector('#hiddencontent');
+
+var show function () {
+    nextStep.classList.toggle('exposed');     
+}
+
+nextStep.addEventListener('click', show);
+
